b847523ead999946b5f85851777b7ccf17e7e926
Will Neto <will_lovinf@LIVE.COM>
2023-03-14 19:45:17 -0300

    feat: adicionando link para exibição de bandeira
    foi criado um dicionário com as pistas default para um de/para de bandeiras baseados no nome do país

--- src/FormulaOneInfo/Pages/Circuits/Circuits.razor.cs
+++ src/FormulaOneInfo/Pages/Circuits/Circuits.razor.cs
@@ -5,6 +5,37 @@ namespace FormulaOneInfo.Pages.Circuits
         public bool Loading = true;
         public List<ApplicationCore.Models.Circuit.Circuit> CircuitsInfo = new();
 
+        public Dictionary<string, string> Flags = new()
+        {
+            { "Germany", "de" },
+            { "Australia", "au" },
+            { "Bahrain", "bh" },
+            { "Vietnam", "vn" },
+            { "China", "cn" },
+            { "Netherlands", "nl" },
+            { "Spain" , "es" },
+            { "Monaco", "mc" },
+            { "Azerbaijan", "az" },
+            { "Canada", "ca" },
+            { "France", "fr" },
+            { "Austria", "at" },
+            { "Great Britain", "gb" },
+            { "Hungary", "hu" },
+            { "Belgium", "be" },
+            { "Italy", "it" },
+            { "Singapore", "sg" },
+            { "Russia", "ru" },
+            { "Japan", "jp" },
+            { "USA", "us" },
+            { "Mexico", "mx" },
+            { "Brazil", "br" },
+            { "Abu Dhabi", "ad" },
+            { "Portugal", "pt" },
+            { "Turkey", "tr" },
+            { "Saudi Arabia", "sa" },
+            { "Qatar", "qa" }
+        };
+
         protected override async Task OnInitializedAsync()
         {
             await ListCircuitsAsync();
